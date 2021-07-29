3fcacb2ad0d7c210be6186dd0563c4d4b65ce013
Felix Bytow <drako@drako.guru>
2021-07-29 07:42:14 +0200

    :recycle: some adjustments to the thread safe locale

--- examples/thread_safe_locale_initialization.cxx
+++ examples/thread_safe_locale_initialization.cxx
@@ -1,9 +1,15 @@
 #include <clocale>
 
-
+// Singleton Design Pattern
+// CAUTION: This pattern is mostly considered an anti pattern
+// as it comes with a lot of downsides. This is a rare case, where it could be okay.
 class LocaleInitializer {
 public:
 	static LocaleInitializer& instance() {
+		// function local statics are initialized exactly once
+		// even across multiple threads
+		// this happends lazily when the first threads calls the
+		// surrounding function
 		static LocaleInitializer inst;
 		return inst;
 	}
@@ -14,17 +20,13 @@ private:
 	}
 };
 
+int foo() {
+	LocaleInitializer::instance();
+}
 
 void initializeLocaleOnce() {
 	// IIFE - Immediatly Invoked Function Expression
 	static auto const initialized = [] {
 		return std::setlocale(LC_ALL, "de_DE.UTF-8");
 	}();
-}
-
-
-int foo() {
-	LocaleInitializer::instance();
-
-
 }
